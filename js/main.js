a4b41a8b46e4d5b4fb354b823af3c957d8af8209
hellstar77 <104196534+hellstar77@users.noreply.github.com>
2022-04-22 11:54:30 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+/*var a = "Хуйло";
+var b = "Привет, ";
+var db = "World!"
+var c = true;
+var d = 5;*/
+
+/*
+prompt("Хто такий Путін")
+
+
+if (a == "Хуйло") {
+    alert("Бінгоооо");
+ }else{
+    alert("false")
+
+}
+*/
+
+/*
+for (var i = 0; i < 5; i++) {
+    alert(i)
+}*/
+/*    var obj = {
+        "key": "value",
+        "key": "value",
+        "key": "value",
+        "key": "value",
+    }
+    */
+/*
+let btnHeader = document.getElementById('btn-header')
+
+    btnHeader.onclick = function () {
+    var text = document.querySelector ('.intro');
+    text.classList.add('red');
+    var img = document.querySelector('.travel');
+    img.style.display = 'none';
+    document.querySelector('.futures').style.marginLeft = '50px';
+}
+
+*/
